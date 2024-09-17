a4b1c0b21b5448946af29272339d9db6224584d4
CasBex <123587431+CasBex@users.noreply.github.com>
2024-09-17 19:35:05 +0200

    Ci updates (#10)
    - Use docker image to compile FMUs with static OpenModelica version
    - Add related CI
    - Set Julia compat less strict
    - CI tests with multiple Julia versions

---
+++ deps/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd deps
+mkdir -p build
+cp src/* build -r
+cd build
+for d in */; do
+    cd $d;
+    omc buildFMU.mos;
+    cd ..;
+done;
+cd ..
+mkdir -p fmu
+find build -iname '*.fmu' -exec mv {} fmu \;
+tar -czf fmu.tar.gz fmu
+cd ..
